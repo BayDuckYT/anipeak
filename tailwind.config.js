2b1e80bfc519378731f600951ab15690dc573f9d
BayDuckYT <therex1752@gmail.com>
2026-04-24 18:45:59 +0300

    Gereksiz safelist uyarilari kaldirildi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  safelist: [
-    { pattern: /^avatar-effect-/ },
-    { pattern: /^comment-effect-/ },
-    { pattern: /^nametag-effect-/ }
-  ],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
