71c9b7f9e1b7667621e8a937b4505ccce9848929
BayDuckYT <therex1752@gmail.com>
2026-06-01 16:33:42 +0300

    Update maintenance logo to v2 and background image

--- src/App.jsx
+++ src/App.jsx
@@ -139,7 +139,7 @@ function MaintenanceScreen({ onAuthOpen }) {
         {/* Logo */}
         <div className="flex items-center gap-3">
           <img 
-            src="/mahorapeaklogo.png" 
+            src="/mahorapeaklogov2.png" 
             alt="MahoraPeak Logo" 
             className="h-32 md:h-40 w-auto object-contain" 
             style={{ 
